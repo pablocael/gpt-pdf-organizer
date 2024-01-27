bf3de8930ed9d66ad2b5ec3cedfdc4149faea6f1
Pablo Carneiro Elias <pablo.cael@gmail.com>
2024-01-27 19:00:32 +0900

    Implemented changed config

--- gpt_pdf_organizer/utils/config.py
+++ gpt_pdf_organizer/utils/config.py
@@ -13,5 +13,5 @@ def read_config():
 config = read_config()
 
 GPT_MODEL_NAME = config.get("gptModelName")
-GPT_API_KEY = config.get("gptApiKey", os.environ.get("OPENAI_API_KEY"))
+GPT_API_KEY = os.environ.get("OPENAI_API_KEY", config.get("gptApiKey", ""))
 MAX_NUM_TOKENS = config.get("maxNumTokens", 1000)
